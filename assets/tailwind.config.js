ab6ebc44a58850688eb955961f1c64e858df57e2
Andreas Eriksson <andreas@codered.se>
2021-10-18 09:57:10 +0200

    Install DaisyUI

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
   ],
   plugins: [
     require('@tailwindcss/forms'),
-    require('@tailwindcss/typography')
+    require('@tailwindcss/typography'),
+    require('daisyui')
   ],
 }
